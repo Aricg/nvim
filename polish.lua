026e3968863b34fea5d200f49a390cb107228ea7
Aric Gardner <aric.gardner@aifi.com>
2025-02-13 15:22:02 -0500

    dont treat helm yaml files as yaml files

--- polish.lua
+++ polish.lua
@@ -4,11 +4,9 @@
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Set up custom filetypes
 vim.filetype.add({
   pattern = {
-    ["*/templates/*.yaml"] = "helm",  -- Match files in any templates directory
-    ["**/templates/*.yaml"] = "helm", -- Match nested templates directories
-    ["*.helm/*.yaml"] = "helm",       -- Match helm chart files
+    [".*/templates/.*%.yaml"] = "helm", -- match any path with /templates/ and ending in .yaml
+    [".*%.helm/.*%.yaml"] = "helm", -- match any path with .helm/ in it and ending in .yaml
   },
-}, { prepend = true }) -- Important: prepend to override default yaml detection
+}, { prepend = true })
